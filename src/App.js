9e2347fa5f470806f60b31b863a1906d04f4f13b
CameronTwiggs <cameron.020617@gmail.com>
2022-08-10 13:22:37 -0400

    adds basic code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,54 @@
-export default function App() {
-  return (<h1> You're a nerd </h1>);
-}
+import React, { useEffect, useState } from "react";
 
+const calculateTimeLeft = () => {
+  let year = new Date().getFullYear();
+  let difference = +new Date(`9/23/${year}`) - +new Date();
+
+  let timeLeft = {};
+
+  if (difference > 0) {
+    timeLeft = {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+
+  return timeLeft;
+};
+
+export default function App() {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+  
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+    return (
+      <div>
+        {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+    </div>
+  );
+}
