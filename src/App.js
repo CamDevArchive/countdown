e000d6742751a280798f9f74d102a0484282af28
CameronTwiggs <cameron.020617@gmail.com>
2022-08-13 00:09:39 -0400

    adds Volunterring Hours and Removes Re-design project

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ export default function App() {
       <h1>Countdown</h1>
       <ul>
         <Timer title="Graduation Day" date={"9/23/"} />
-        <Timer title="Redesign Project Day" date={"8/12/"} />
         <Timer title="Portfolio Project" date={"8/31/"} />
+        <Timer title="Volunteering Hours" date={"9/16/"} />
       </ul>
       <p>You keep checking in Im going to mess with the css. or maybe ask for help :D </p>
     </>
