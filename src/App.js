bc90d12e2dadc33e217f090baebd323fa6165a42
CameronTwiggs <cameron.020617@gmail.com>
2022-09-06 15:04:24 -0400

    adds new assignments and removes an old one

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
       <Header />
       <ul className='timerList'>
         <Timer title="Graduation Day" date={"9/23/"} />
-        <Timer title="Portfolio Project" date={"8/31/"} />
+        <Timer title="Restarant Project" date={"9/14/"} />
         <Timer title="Volunteering Hours" date={"9/16/"} />
       </ul>
       <p>You're a nerd. :D </p>
